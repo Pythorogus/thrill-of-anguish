f8507df1a01f413a5e6561d8511ac5a5a7115751
Pythorogus <pierre.gomba@gmail.com>
2025-05-12 17:31:53 +0200

    startscreen

---
+++ scripts/fade_in_label.gd
@@ -0,0 +1,43 @@
+extends Node
+
+@onready var title_label: Label = $"../TitleLabel"
+@onready var press_enter_label: Label = $"../PressEnterLabel"
+@onready var title_screen_music: AudioStreamPlayer = $"../TitleScreenMusic"
+@onready var enter_sfx: AudioStreamPlayer = $"../EnterSFX"
+@onready var fade_out: ColorRect = $"../FadeOut"
+
+var fadeOutMusic = false
+
+func _ready():
+	# Démarre invisible
+	fade_out.modulate.a = 0.0
+	title_label.modulate.a = 0.0  
+	press_enter_label.modulate.a = 0.0
+
+	var timer = get_tree().create_timer(2.0)  # 3 secondes de délai avant de commencer le fade-in
+	await timer.timeout
+
+	var tween = create_tween()
+	tween.tween_property(title_label, "modulate:a", 1.0, 1.0)  # Vers alpha = 1 sur 1 seconde
+
+	var timer2 = get_tree().create_timer(2.0)  # 3 secondes de délai avant de commencer le fade-in
+	await timer2.timeout
+
+	pressEnterAnimation()
+
+func pressEnterAnimation():
+	var tween = create_tween()
+	tween.tween_property(press_enter_label, "modulate:a", 1.0, 1.0)
+	tween.tween_property(press_enter_label, "modulate:a", 0.0, 1.0)
+	tween.finished.connect(func(): pressEnterAnimation())
+	
+func _process(delta: float)->void:
+	if Input.is_action_just_pressed("enter"):
+		enter_sfx.play()
+		var tween = create_tween()
+		tween.tween_property(title_screen_music, "volume_db", -80, 2.0)
+		var tween2 = create_tween()
+		tween2.tween_property(fade_out, "modulate:a", 1.0, 2.0)
+		var timer = get_tree().create_timer(3.0)
+		await timer.timeout
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,12 +1,21 @@
 extends Node
 
-var last_checkpoint
+@onready var fade_in: ColorRect = $"../Player/Camera2D/FadeIn"
+@onready var camera: Camera2D = $"../Player/Camera2D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	update_colorrect_size()
+	var tween = create_tween()
+	tween.tween_property(fade_in, "modulate:a", 0.0, 1.0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	update_colorrect_size()
+
+func update_colorrect_size()->void:
+	# Récupérer la taille de la fenêtre de jeu (viewport)
+	var screen_size = get_viewport().size
+	fade_in.custom_minimum_size = screen_size
+	fade_in.position = camera.position
